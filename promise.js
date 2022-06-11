59775cfb7fd6dfbc23013a447ca0b0c17b0de610
dedy270895 <dedospratamos@gmail.com>
2022-06-12 00:16:38 +0700

    membuat fungsi memanggil promise dengan async

--- promise.js
+++ promise.js
@@ -1,7 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi)=>{
+  try{
+    const xxi = await promiseTheaterIXX();
+    const cgv = await promiseTheaterVGC();
+    const allBisokop = [...xxi, ...cgv];//spread
+
+    //filter by emosi
+    // Object.filter = (obj, predicate) => 
+    //   Object.keys(obj)
+    //         .filter( key => predicate(obj[key]) )
+    //         .reduce( (res, key) => (res[key] = obj[key], res), {} );
+
+    //const hasil = filterObj(emosi,allBisokop);
+    //var filtered = Object.filter(allBisokop, hasil => hasil== 'marah'); 
+    let hasil= [] ;
+    allBisokop.forEach((Element)=>{
+      if(Element.hasil == emosi){
+        hasil.push(Element.hasil);
+      }
+    })
+    const jumlah = hasil.length;
+    return jumlah;
+    
+  }catch(rejectMessage){
+    console.log(rejectMessage);
+  }
+};
 
 module.exports = {
   promiseOutput,
